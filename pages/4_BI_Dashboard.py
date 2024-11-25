b7d187ab0adc1575d1a5e9e2fe897a25b4b7bd02
Jose Ibarra <jiibarra@uc.cl>
2024-11-25 11:15:45 -0300

    BI dashboard summary and charts

--- pages/4_BI_Dashboard.py
+++ pages/4_BI_Dashboard.py
@@ -22,5 +22,52 @@ if "data" not in st.session_state:
 # Load the dataset from session state
 data = st.session_state["data"]
 
+#Date columns added to data
+data["trans_date_trans_time"] = pd.to_datetime(data["trans_date_trans_time"], errors='coerce')
+data['date'] = data["trans_date_trans_time"].dt.date
+data['month'] = data["trans_date_trans_time"].dt.month_name()
+data['year'] = data["trans_date_trans_time"].dt.year
+
 # Display the first few rows of the dataset for user overview
 st.write(data.head())
+
+# Sidebar filter
+with st.sidebar:
+    st.title('Filters')
+    year_list = list(data["year"].dropna().unique())[::-1]
+    month_list = list(data["month"].dropna().unique())[::-1]
+    state_list = list(data["state"].sort_values().dropna().unique())
+    selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
+    selected_month = st.selectbox('Select a month', month_list, index=len(year_list)-1)
+    selected_state = st.selectbox('Select a state', state_list, index=len(year_list)-1)
+
+# Yearly summary metrics
+year_transactions = data.loc[data['year'] == selected_year]['amt'].count()
+year_transactions_amt = data.loc[data['year'] == selected_year]['amt'].sum()
+avg_monthly_transactions = year_transactions/len(data.loc[data['year']==selected_year]['month'].unique())
+avg_monthly_ftransactions = data.loc[(data['year'] == selected_year) & (data['is_fraud'] == 1)].shape[0]
+avg_monthly_tamount = data.loc[(data['year'] == selected_year)]['amt'].sum()/len(data.loc[data['year']==selected_year]['month'].unique())
+avg_monthly_famount = data.loc[(data['year'] == selected_year) & (data['is_fraud'] == 1)]['amt'].sum()/len(data.loc[data['year']==selected_year]['month'].unique())
+
+print(data.loc[data['year']==selected_year]['month'].unique())
+
+# Yearly summary display
+st.header(f'{selected_year} Summary', divider='gray')
+col0, col1, col2 = st.columns(3)
+
+with col0:
+    st.metric(label =f"Total transactions", value = '{:,}'.format(year_transactions))
+    st.metric(label =f"Transacted amount", value = str('$' + '{:,}'.format(round(year_transactions_amt/1000))) + 'k')
+
+with col1:
+    st.metric(label ="Avg. monthly transactions", value = '{:,}'.format(round(avg_monthly_transactions)))
+    st.metric(label ="Avg. monthly transacted amount", value = ('$' + '{:,}'.format(round(avg_monthly_tamount/1000))) + 'k')
+
+with col2:
+    st.metric(label = "Avg. fraudulent transactions", value= '{:,}'.format(avg_monthly_ftransactions))
+    st.metric(label = "Avg. monthly fraudulent transacted amount", value = ('$' + '{:,}'.format(round(avg_monthly_famount/1000))) + 'k')
+
+yearly_line_chart = data.loc[data['year']==selected_year].groupby("date")['amt'].sum()
+st.line_chart(yearly_line_chart)
+fyearly_line_chart = data.loc[(data['year']==selected_year) & (data['is_fraud']==1)].groupby("date")['amt'].sum()
+st.line_chart(fyearly_line_chart)
