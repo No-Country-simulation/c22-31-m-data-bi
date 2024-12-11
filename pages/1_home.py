bccf52087ca3042907839df3a7e8df33a3590f72
Jose Ibarra <jiibarra@uc.cl>
2024-12-11 12:17:06 -0300

    aesthetic update

--- pages/1_home.py
+++ pages/1_home.py
@@ -1,25 +1,47 @@
 import streamlit as st
 import pandas as pd
 import os
-
+import base64
 
 # Load CSS file
 def load_css():
+    """Carga el archivo CSS en la aplicación de Streamlit."""
     css_path = os.path.join("styles", "styles.css")
     with open(css_path, "r") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+def add_logo_to_header():
+    """Agrega el logo al encabezado utilizando la clase definida en CSS."""
+    logo_path = os.path.join("static", "riskova_logo.png")
+    logo_base64 = get_base64_image(logo_path)
+    st.markdown(
+        f"""
+        <style>
+        [data-testid="stHeader"]::before {{
+            background-image: url('data:image/png;base64,{logo_base64}');
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
+def get_base64_image(image_path):
+    """Convierte una imagen local en un string Base64."""
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode("utf-8")
+
+# load css file and add logo
 load_css()
+add_logo_to_header()
 
-
-st.title("Welcome to Fraud Detection System")
-st.write("Description of the project...")
+# Description
+st.title("Welcome to Riskova!")
+st.write("The transaction tracking system that helps you identifying fraudulent activity through data science and machine learning techniques.")
 
 # Path for the temporary file
 TEMP_FILE_PATH = "data/dataset.csv"
 
-st.write("Upload a CSV file to start the analysis.")
+st.write("Upload a CSV file of your company's transaction records to start the analysis.")
 
 # Load Data
 uploaded_file = st.file_uploader("Select a CSV file.", type=["csv"])
